108e4a7401714509b4f4d220cf43993e00699f77
Kavindu Maleesha <kaviigunawardhana2000@gmail.com>
2024-02-15 19:48:14 +0530

    Update
    Update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,7 +15,12 @@ const Hero = () => {
 
        {/* Hero Heading */}
        <div className="hero-text">
-
+      <div>
+        <span>Shape </span>
+        <span>Your</span>
+        </div>
+        <div><span>
+          Ideal body</span></div>
         </div>
       </div>
       <div className="right-h">right side</div>
