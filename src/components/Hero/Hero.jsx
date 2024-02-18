6cdd9f785897e7769ab28e5f25db1f94ee960c9d
Kavindu Maleesha <kaviigunawardhana2000@gmail.com>
2024-02-18 18:47:49 +0530

    Update
    Update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,16 +12,21 @@ const Hero = () => {
           <span>the best fitness club in the town</span>
          </div>
   
-  
          {/* Hero Heading */}
          <div className="hero-text">
         <div>
-          <span>Shape </span>
+          <span className='stroke-text'>Shape </span>
           <span>Your</span>
           </div>
-          <div><span>
-            Ideal body</span></div>
+          <div>
+            <span>Ideal body</span>
           </div>
+          <div>
+            <span>
+          In here we will help you to shape and build your ideal body and live up your life to fullest
+            </span>
+        </div>
+        </div>
         </div>
         <div className="right-h">right side</div>
       </div>
