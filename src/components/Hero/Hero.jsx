56306460c5cecd62617face124be20757208b206
Kavindu Maleesha <kaviigunawardhana2000@gmail.com>
2024-02-20 13:26:53 +0530

    Update
    Update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Header from '../Header/Header'
 import './Hero.css'
+import hero_image from "../../assets/hero_image.png"
+import hero_image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
 const Hero = () => {
     return (
       <div className="hero">
@@ -44,11 +48,24 @@ const Hero = () => {
             <span>fitness programs</span>
             </div>
         </div>
-        
+
+        {/* hero buttons */}
+        <div className="hero-buttons">
+          <buttons className="btn">Get Started</buttons>
+          <buttons className="btn">Learn More</buttons>
         </div>
-        <div className="right-h">right side</div>
+        </div>
+        <div className="right-h">
+          <button className='btn'>Join Now</button>
+
+          <div className="heart-rate">
+            <img src={Heart} alt="" />
+            <span>Heart Rate</span>
+            <span>116 bpm</span>
+          </div>
+          </div>
       </div>
-    )
-  }
+    );
+  };
   
   export default Hero
