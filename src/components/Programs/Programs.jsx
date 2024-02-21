b338bbd1b1904758cee7836c879876c4b8accefb
Kavindu Maleesha <kaviigunawardhana2000@gmail.com>
2024-02-21 13:55:22 +0530

    Update
    Update

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import './Programs.css'
-export const Programs = () => {
+import React from 'react';
+import './Programs.css';
+import {programsData} from '../../data/programsData';
+import RightArrow from '../../assets/rightArrow.png';
+const Programs = () => {
   return (
     <div className="Programs" id="programs">
         {/* header */}
@@ -9,8 +11,19 @@ export const Programs = () => {
             <span>Programs</span>
             <span className='stroke-text'>to shape you</span>
         </div>
-    </div>
-  )
-}
 
-export default Programs
+        <div className="program-categories">
+            {programsData.map((program)=>(
+                <div className="category">
+                    {program.image}
+                    <span>{program.heading}</span>
+                    <span>{program.details}</span>
+                    <div className="join-now"><span>Join Now</span><img src={RightArrow} alt=""/></div>
+                </div>
+            ))}
+        </div>
+    </div>
+  );
+};
+
+export default Programs;
