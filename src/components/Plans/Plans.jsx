0b1baa9803f963fe720038ccde6ed8eedb67ddeb
Kavindu Maleesha <kaviigunawardhana2000@gmail.com>
2024-02-25 18:51:54 +0530

    Updates
    Updates

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {plansData} from '../../data/plansData'
+import {plansData} from '../../data/plansData';
+import whiteTick from '../../assets/whiteTick.png'
 import './Plans.css';
 const Plans = () => {
   return (
@@ -12,7 +13,22 @@ const Plans = () => {
 
         {/* plans card */}
         <div className="plans">
+          {plansData.map((plan, i)=> (
+            <div className="plan" key={i}>
+              {plan.icon}
+              <span>{plan.name}</span>
+              <span>$ {plan.price}</span>
 
+              <div className="features">
+                {plan.features.map((feature, i)=> (
+                  <div className="feature">
+                    <img src={whiteTick} alt="" />
+                    <span key={i}>{feature}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
     </div>
   );
