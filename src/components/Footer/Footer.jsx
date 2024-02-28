cfad229a39169e5dd586a87a2f08ab64b537814a
Kavindu Maleesha <kaviigunawardhana2000@gmail.com>
2024-02-28 23:03:08 +0530

    Update
    Update

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css';
+import Github from "../../assets/github.png";
+import Instagram from "../../assets/instagram.png.png";
+import LinkedIn from "../../assets/linkedin.png.png";
+import Logo from "../../assets/logo.png.png";
+const Footer = () => {
+  return (
+    <div className="Footer-container">
+        <hr/>
+       <div className="footer">
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        </div> 
+    </div>
+  )
+}
+
+export default Footer ;
