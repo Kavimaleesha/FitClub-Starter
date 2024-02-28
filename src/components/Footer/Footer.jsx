4d655adb7f1cc0aa5b96c116a2598c347707b3e2
Kavindu Maleesha <kaviigunawardhana2000@gmail.com>
2024-02-28 23:24:02 +0530

    Update
    Update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import './Footer.css';
 import Github from "../../assets/github.png";
-import Instagram from "../../assets/instagram.png.png";
-import LinkedIn from "../../assets/linkedin.png.png";
-import Logo from "../../assets/logo.png.png";
+import Instagram from "../../assets/instagram.png";
+import LinkedIn from "../../assets/linkedin.png";
+import Logo from "../../assets/logo.png";
 const Footer = () => {
   return (
     <div className="Footer-container">
         <hr/>
        <div className="footer">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <div className="social-links">
+        <img src={Github} alt="" />
+        <img src={Instagram} alt="" />
+        <img src={LinkedIn} alt="" />
         </div> 
+        <div className="logo-f">
+        <img src={Logo} alt="" />
+        </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer ;
