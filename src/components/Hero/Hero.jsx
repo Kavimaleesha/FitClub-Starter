74f173889ae0653020c5c960186797730b4671e5
Kavindu Maleesha <kaviigunawardhana2000@gmail.com>
2024-02-29 10:46:12 +0530

    29/02/2024
    29/02/2024

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,16 +5,24 @@ import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
+
+import {motion} from "framer-motion"
 const Hero = () => {
+
+  const transition = {type: 'spring',duration:3}
     return (
       <div className="hero">
 
-        <div className="blur"></div>
+        <div className="blur hero-blur"></div>
         <div className="left-h">
           <Header/>
   {/* the best ad */}
           <div className="the-best-ad">
-          <div></div>
+          <motion.div
+            initial={{left: '238px'}}
+            whileInView={{left: '8px'}}
+            transition={{...transition, type: 'tween'}}
+          ></motion.div>
           <span>the best fitness club in the town</span>
          </div>
   
@@ -60,11 +68,14 @@ const Hero = () => {
         <div className="right-h">
           <button className='btn'>Join Now</button>
 
-          <div className="heart-rate">
+          <motion.div 
+          whileInView={{right:'4rem'}}
+          transition={transition}
+          className="heart-rate">
             <img src={Heart} alt="" />
             <span>Heart Rate</span>
             <span>116 bpm</span>
-          </div>
+          </motion.div>
 
           {/* hero images */}
           <img src={hero_image} alt="" className="hero-image" />
