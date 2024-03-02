85df0657ad812d8c5c63aeafe4c11b37a7f9bc29
Kavindu Maleesha <kaviigunawardhana2000@gmail.com>
2024-03-02 17:54:06 +0530

    Update
    Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import'./Header.css'
-import Logo from '../../assets/logo.png'
+import React, { useState } from 'react';
+import'./Header.css';
+import Logo from '../../assets/logo.png';
+import Bars from '../../assets/bars.png';
 const Header = () => {
+  const mobile = window.innerWidth<=768 ? true: false;
+  const[menuOpened, setMenuOpened] = useState(false);
+
     return (
       <div className="header">
-      
-          <img src={Logo} alt="" className='logo'/>
-  
+        <img src={Logo} alt="" className='logo'/>
+  {menuOpened=== false && mobile===true ? (
+    <div
+    style={{
+    backgroundColor: 'var(--appColor)', 
+    padding: '0.5rem',
+    borderRadius: '5px',
+  }}
+    >
+      <img src={Bars} alt="" style={{width: '1.5rem', height: '1.5rem'}}/>
+      </div>
+  ) : (
           <ul className='header-menu'>
             <li>Home</li>
             <li>Programs</li>
@@ -14,8 +27,9 @@ const Header = () => {
             <li>Plans</li>
             <li>Testimonials</li>
           </ul>
+          )}
       </div>
-    )
-  }
+    );
+  };
   
-  export default Header
+  export default Header;
